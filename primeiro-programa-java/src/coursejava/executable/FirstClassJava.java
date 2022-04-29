e364eafbfd36080cdb041765b053763ccee98ccb
gabriel <gabriel@sandbox>
2022-04-29 12:09:20 -0300

    Create project

---
+++ primeiro-programa-java/src/coursejava/executable/FirstClassJava.java
@@ -0,0 +1,11 @@
+package coursejava.executable;
+
+public class FirstClassJava {
+	/*Main é um método auto executavel em Java*/
+	public static void main(String[] args) {
+		
+		/* Imprime no console */
+		System.out.println("My first program in Java");
+
+	}
+}
