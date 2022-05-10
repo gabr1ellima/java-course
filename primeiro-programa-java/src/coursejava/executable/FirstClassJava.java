510e635e9ccaf7d661120277a466b73a1afdeb9c
gabriel <gabriel@sandbox>
2022-05-09 21:18:51 -0300

    Exercise resolved

--- primeiro-programa-java/src/coursejava/executable/FirstClassJava.java
+++ primeiro-programa-java/src/coursejava/executable/FirstClassJava.java
@@ -1,11 +1,27 @@
 package coursejava.executable;
 
+import java.util.Locale;
+
 public class FirstClassJava {
 	/*Main é um método auto executavel em Java*/
 	public static void main(String[] args) {
 		
-		/* Imprime no console */
-		System.out.println("My first program in \nJava");
-
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.printf("Products:%n%s, which price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+		System.out.printf("%nRecord: %d years old, code %d and gender: %c%n", age, code, gender);
+		System.out.printf("%nMeasure with eight decimal places: %.8f\nRouded (three decimal places): %.3f%n", measure, measure);
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f%n", measure);
 	}
 }
